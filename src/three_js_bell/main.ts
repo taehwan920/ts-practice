4498edaf3fdb360363483a738ceb67b7ad465353
Yang Taehwan <taehwan920@gmail.com>
2021-10-24 01:36:06 +0900

    feat: 첫 three.js 예제

--- src/three_js_bell/main.ts
+++ src/three_js_bell/main.ts
@@ -1,28 +1,83 @@
 import * as THREE from '../three.module.js'
 
-function main() {
-  const canvas = document.querySelector('#main_canvas');
-  const renderer = new THREE.WebGLRenderer({ canvas });
+class App {
+  private _divContainer: HTMLDivElement
+  private _renderer: THREE.WebGLRenderer
+  private _scene: THREE.Scene
+  private _camera: THREE.PerspectiveCamera
+  private _cube: any
 
-  const fov = 75;
-  const aspect = 2;  // the canvas default
-  const near = 0.1;
-  const far = 5;
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  constructor() {
+    const divContainer: HTMLDivElement = document.querySelector('#main_canvas')!
+    this._divContainer = divContainer;
 
-  const scene = new THREE.Scene();
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
 
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    this._renderer = renderer;
 
-  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });  // greenish blue
+    const scene = new THREE.Scene();
+    this._scene = scene;
 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
 
-  renderer.render(scene, camera);
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    requestAnimationFrame(this.render.bind(this))
+  }
+
+  private _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera: any = new THREE.PerspectiveCamera(75, width / height, 0.1, 100)
+    camera.position.z = 2;
+    this._camera = camera;
+  }
+
+  private _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    this._scene.add(light);
+  }
+
+  private _setupModel() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material: any = new THREE.MeshPhongMaterial({ color: 0x44a88 });
+
+    const cube = new THREE.Mesh(geometry, material);
+
+    this._scene.add(cube);
+    this._cube = cube;
+  }
+
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._renderer.setSize(width, height);
+  }
+
+  render(time: number) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time: number) {
+    time *= 0.001;
+    this._cube.rotation.x = time;
+    this._cube.rotation.y = time;
+  }
 }
 
-main();
+window.onload = function () {
+  new App()
+}
